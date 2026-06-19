213be338bbc7f859935cf2c73c8612e86745c9cf
Conrado Halmenschlager <90523295+ConradoHVieira@users.noreply.github.com>
2026-06-19 16:23:13 -0300

    commit inicial

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ while True :
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit() # Close window
+            pygame.quit() #  Close window
             quit() # end pygame
