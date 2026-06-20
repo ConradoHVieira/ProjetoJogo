cf01484b7016199b8bb40ffcae06fac060b00dc9
Conrado Halmenschlager <90523295+ConradoHVieira@users.noreply.github.com>
2026-06-20 12:43:14 -0300

    added assets

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,18 @@
+print("EXECUTANDO menu")
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
+        print("CARREGANDO IMAGEM")
         self.window = window
+        self.surf = pygame.image.load("assets/MenuBg.png")
+        print("IMAGEM CARREGADA")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
         pass
